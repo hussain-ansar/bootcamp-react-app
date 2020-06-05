2448c6166351263223aa6c842f31ee30d4ef5f5a
Ansar Hussain <65576958+hussain-ansar@users.noreply.github.com>
2020-06-06 01:02:39 +0500

    Change in App

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi <code>Im a Web Developer</code> and here Code practice.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          PIAC Bootcamp 2020
         </a>
       </header>
     </div>
