4bef5563e7b586afe165577f194ed374525b4116
Ansar Hussain <65576958+hussain-ansar@users.noreply.github.com>
2020-06-10 00:23:31 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Fruits from './Fruits.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Fruits fName="Orange" shakeName ="Banana Shake"/>
-        <br/>
-        <Fruits fName="Cherr" shakeName ="Mango Shake"/>
-        <br/>
-        <Fruits fName="blueberry" shakeName ="Apple juice"/>
         <p>
-          <h1>ZoZo Bootcamp 2020</h1>
+          Hi <code>Im a Web Developer</code> and here Code practice.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          PIAC Bootcamp 2020
+        </a>
       </header>
     </div>
   );
